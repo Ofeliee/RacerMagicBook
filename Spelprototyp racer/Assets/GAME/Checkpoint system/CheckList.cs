fa763c4165bec1d1497d118b32d9412e737c3cff
Ofeliee <paulina_frisk@hotmail.com>
2018-04-27 17:19:37 +0200

    Checkpoint implemented

---
+++ Spelprototyp racer/Assets/GAME/Checkpoint system/CheckList.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckList : MonoBehaviour {
+
+    public int nrOfCheckpoints = 0;
+    public int currentCheckpoint = 0;
+    public int currentLap;
+    public Vector3 startPos;
+    public int lap;
+    private GameObject[] arr;
+    public List<GameObject> checkpoints = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+
+        // Player start position
+        startPos = transform.position;
+        currentCheckpoint = 0;
+        currentLap = 0;
+
+        // Find all checkpoint gameobjects, placed in arr and then over to List.
+        arr = GameObject.FindGameObjectsWithTag("Checkpoint");
+        nrOfCheckpoints = arr.Length;
+        print(nrOfCheckpoints);
+
+        for (int i = 0; i < nrOfCheckpoints; i++)
+        {
+            checkpoints.Add(arr[i]);
+        }
+
+        for (int i = 0; i < nrOfCheckpoints; i++)
+        {
+            if(i != checkpoints[i].GetComponent<Index>().index)
+            {
+                checkpoints.Insert(checkpoints[i].GetComponent<Index>().index, checkpoints[i]);
+                checkpoints.RemoveAt(i);
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //lap = currentLap;
+	}
+}
---
+++ Spelprototyp racer/Assets/GAME/Checkpoint system/CheckPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    public int cPassed = 0;
+    Vector3 currentPos;
+    Quaternion currentRot;
+    public GameObject reefref;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Get the checkpoint element in the list. and put the the element nr in index
+        if (other.CompareTag("Checkpoint"))
+        {
+                if (other.gameObject.GetComponent<Index>().index == cPassed)
+                {
+                    // Gets the checkpoints location and increase current checkpoint by 1 
+                    GetLocation(other);
+                    cPassed += 1;
+                    print("HIT");
+                }
+        }
+
+    }
+    private void GetLocation(Collider checkpoint)
+    {
+        currentPos = checkpoint.GetComponent<Transform>().position;
+        currentRot = checkpoint.GetComponent<Transform>().rotation;
+    }
+}
