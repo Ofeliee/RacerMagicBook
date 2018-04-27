eaed9859e6a3620dff0c6f1729b731b0e64acfd0
Ofeliee <paulina_frisk@hotmail.com>
2018-04-27 14:07:54 +0200

    Derp till checkpoint system

---
+++ Spelprototyp racer/Assets/GAME/Checkpoint system/CheckList.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckList : MonoBehaviour {
+
+    int nrOfCheckpoints = 0;
+    public int currentCheckpoint = 0;
+    public int currentLap;
+    public Vector3 startPos;
+    public int lap;
+    private GameObject[] arr;
+    public List<GameObject> checkpoints = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+
+        // Player start position
+        startPos = transform.position;
+        currentCheckpoint = 0;
+        currentLap = 0;
+
+        // Find all checkpoint gameobjects, placed in arr and then over to List.
+        arr = GameObject.FindGameObjectsWithTag("Checkpoint");
+        nrOfCheckpoints = arr.Length;
+
+        for (int i = 0; i < nrOfCheckpoints; i++)
+        {
+            checkpoints.Add(arr[i]);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //lap = currentLap;
+	}
+}
---
+++ Spelprototyp racer/Assets/GAME/Checkpoint system/CheckPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    public int index = -1;
+    Vector3 currentPos;
+    public GameObject reefref;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Get the checkpoint element in the list. and put the the element nr in index
+        index += 1; 
+
+        if((index > reefref.GetComponent<CheckList>().currentCheckpoint))
+        {
+            if(other.CompareTag("Checkpoint"))
+            {
+                Debug.Log("Hit!");
+                // Gets the checkpoints location and increase current checkpoint by 1 
+                GetLocation(other);
+                reefref.GetComponent<CheckList>().currentCheckpoint += 1; 
+            }
+        }
+
+    }
+
+    private void GetLocation(Collider player)
+    {
+        currentPos = transform.position;
+    }
+}
